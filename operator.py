67e5abf9db8bf87ee6c9bfb799cc71b5417e756c
Nicolas St-Laurent <stlaurent.nicolas@gmail.com>
2022-06-08 16:06:18 -0400

    Add operators example

---
+++ operator.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# Assignment: =
+base = 12
+print("base = 12 : "+str(base))
+
+# Arithmetic: + - * / % // **
+print("base+base : "+str(base+base))
+print("base-base : "+str(base-base))
+print("base*base : "+str(base*base))
+print("base/base : "+str(base/base))
+print("base%%base : "+str(base%base))
+print("base//base : "+str(base//base))
+print("base**base : "+str(base**base))
+
+# Comparison: == != >= > <= <
+print("base==12 : "+str(base==12))
+print("base!=12 : "+str(base!=12))
+print("base>=12 : "+str(base>=12))
+print("base>12 : "+str(base>12))
+print("base<=12 : "+str(base<=12))
+print("base<12 : "+str(base<12))
+
+# Logical: and or not
+print("True and False : "+str(True and False))
+print("True or False : "+str(True or False))
+print("not True : "+str(not True))
