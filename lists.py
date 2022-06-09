334fd38341582f6c9b5465b1240948d5cb0ba55a
Nicolas St-Laurent <stlaurent.nicolas@gmail.com>
2022-06-09 11:16:18 -0400

    Add list example

---
+++ lists.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+inventory = ["Axe", "Food", "Helmet"]
+inventory_size = len(inventory)
+
+print(inventory)
+print("Inventory size: "+str(inventory_size))
+print("First two: "+str(inventory[0:2]))
+print("Last: "+inventory[inventory_size-1])
